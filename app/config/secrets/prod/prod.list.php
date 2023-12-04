b32769c316628f7cbf1eb3834522e4399643c7bb
Jan-Peter Wittig <jan-peter.wittig@live.com>
2023-12-04 12:58:36 +0100

    Initial Prod Deployment

---
+++ app/config/secrets/prod/prod.list.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'MYSQL_DBNAME' => null,
+    'MYSQL_HOST' => null,
+    'MYSQL_PASSWORD' => null,
+    'MYSQL_TEST_DBNAME' => null,
+    'MYSQL_TEST_HOST' => null,
+    'MYSQL_TEST_PASSWORD' => null,
+    'MYSQL_TEST_USER' => null,
+    'MYSQL_USER' => null,
+];
