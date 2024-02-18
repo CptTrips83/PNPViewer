317816642df638fdf1956650509842f8b241d39e
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-02-18 19:39:29 +0100

    Add CharacterEditController with editing functionality
    This update introduces the CharacterEditController that handles character stat value and class level editing. It also validates Ajax requests and interacts directly with the character data in the database. The controller takes care of sending a new value to the CharacterEditorFactory and saves updated character data.
    
    Signed-off-by: Jan-Peter Wittig <jan-peter.wittig@live.com>

---
+++ app/src/Controller/CharacterEditController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\CharacterClassLevel;
+use App\Entity\CharacterData;
+use App\Entity\CharacterStatValue;
+use App\Tools\Character\Factory\CharacterEditorFactory;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/character/edit', name: 'app_character_edit')]
+class CharacterEditController extends AbstractController
+{
+    #[Route('/stat', name: '.stat')]
+    public function editStatValue(
+        Request $request,
+        EntityManagerInterface $entityManager): Response
+    {
+        if(!$request->isXmlHttpRequest()) return new Response("", "400");
+
+        $statValueId = $request->request->get("statValueId");
+        $newValue = $request->request->get("newValue");
+
+        $repoCharacterStatValue = $entityManager->getRepository(CharacterStatValue::class);
+
+        /** @var CharacterStatValue $characterStatValue */
+        $characterStatValue = $repoCharacterStatValue->find($statValueId);
+
+        /** @var CharacterData $character */
+        $character = $characterStatValue->getCharacterData();
+
+        $characterEditor = CharacterEditorFactory::get($entityManager, $character->getRuleSet());
+
+        $characterEditor->setCharacter($character)
+            ->setStatValue($characterStatValue->getCharacterStat(), $newValue)
+            ->saveCharacter();
+
+        return new Response("", "200");
+    }
+
+    #[Route('/stat', name: '.class')]
+    public function editClassValue(
+        Request $request,
+        EntityManagerInterface $entityManager): Response
+    {
+        if(!$request->isXmlHttpRequest()) return new Response("", "400");
+
+        $classLevelId = $request->request->get("classLevelId");
+        $newValue = $request->request->get("newValue");
+
+        $repoCharacterClassLevel = $entityManager->getRepository(CharacterClassLevel::class);
+
+        /** @var CharacterClassLevel $characterClassLevel */
+        $characterClassLevel = $repoCharacterClassLevel->find($classLevelId);
+
+        /** @var CharacterData $character */
+        $character = $characterClassLevel->getCharacterData();
+
+        $characterEditor = CharacterEditorFactory::get($entityManager, $character->getRuleSet());
+
+        $characterEditor->setCharacter($character)
+            ->setClassLevel($characterClassLevel->getCharacterClass(), $newValue)
+            ->saveCharacter();
+
+        return new Response("", "200");
+    }
+}
