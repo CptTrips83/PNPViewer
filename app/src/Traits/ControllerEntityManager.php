a4049dfae3763135e25a8b695cdf935519310014
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-02-19 17:22:20 +0100

    Update event handling and enhance error handling in detailsEdit and Ajax
    Refined the "change" event handling in detailsEdit, adding additional error checks. Improved error handling logic in Ajax request handling, broadening the range of successful status codes. Also, wrapped the process in try-catch blocks to handle and log potential errors. Enhanced code documentation in both files, providing clearer information for each function.
    
    Signed-off-by: Jan-Peter Wittig <jan-peter.wittig@live.com>

--- app/src/Traits/ControllerEntityManager.php
+++ app/src/Traits/ControllerEntityManager.php
@@ -4,10 +4,21 @@ namespace App\Traits;
 
 use Doctrine\ORM\EntityManagerInterface;
 
+/**
+ * Trait ControllerEntityManager.
+ *
+ * This trait provides methods for managing the entity manager.
+ */
 trait ControllerEntityManager
 {
     private EntityManagerInterface $_entityManager;
 
+    /**
+     * Sets the entity manager.
+     *
+     * @param EntityManagerInterface $entityManager The entity manager instance.
+     * @return void
+     */
     private function setEntityManager(EntityManagerInterface $entityManager) : void
     {
         $this->_entityManager = $entityManager;
--- app/src/Traits/ControllerForm.php
+++ app/src/Traits/ControllerForm.php
@@ -8,13 +8,29 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Trait ControllerForm
+ *
+ * This trait provides methods for processing forms in a controller.
+ */
 trait ControllerForm
 {
+    /**
+     * Process a form.
+     *
+     * @param EntityManagerInterface $entityManager The entity manager.
+     * @param Request $request The request object.
+     * @param string $entityClassName The fully qualified class name of the entity to be processed.
+     * @param string $formClassName The fully qualified class name of the form to be created.
+     * @param int $id The ID of the entity (optional, default value is 0).
+     *
+     * @return FormInterface The processed form.
+     */
     private function processForm(EntityManagerInterface $entityManager,
-                                 Request $request,
-                                 string $entityClassName,
-                                 string $formClassName,
-                                 int $id = 0) : FormInterface
+                                 Request                $request,
+                                 string                 $entityClassName,
+                                 string                 $formClassName,
+                                 int                    $id = 0) : FormInterface
     {
         $dataObj = new $entityClassName();
 
@@ -41,6 +57,16 @@ trait ControllerForm
         return $form;
     }
 
+    /**
+     * Redirects to a given route if a form submission is completed and valid.
+     *
+     * @param FormInterface $form The form object to check.
+     * @param string $redirect The route to redirect to on form completion.
+     * @param array $parameter Optional parameters to pass to the redirect route.
+     *
+     * @return Response|null Returns a Response object if the form submission is completed and valid,
+     *                      or null if the form is not yet completed or invalid.
+     */
     private function redirectOnFormCompletion(FormInterface $form, string $redirect, array $parameter = []) : Response | null
     {
         if($form->isSubmitted() && $form->isValid()) {
