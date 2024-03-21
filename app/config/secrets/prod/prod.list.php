bb9ae39c940b910f39343846616cc382e3f5265b
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-03-21 18:56:49 +0100

    Add new mailer configuration files and settings
    Four new configuration files for mailer settings - MAILER_USER, MAILER_PASSWORD, MAILER_SMTP, MAILER_PORT - are added in the production secrets. This includes the update of both 'prod.list.php' and 'mailer.yaml' files to accommodate these new settings. The purpose of these changes is to enable SMTP mail sending in a production environment while maintaining null mail sending in development and test environments.
    
    Signed-off-by: Jan-Peter Wittig <jan-peter.wittig@live.com>

--- app/config/secrets/prod/prod.list.php
+++ app/config/secrets/prod/prod.list.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'MAILER_PASSWORD' => null,
+    'MAILER_PORT' => null,
+    'MAILER_SMTP' => null,
+    'MAILER_USER' => null,
     'MYSQL_DBNAME' => null,
     'MYSQL_HOST' => null,
     'MYSQL_PASSWORD' => null,
